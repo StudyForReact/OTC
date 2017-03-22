a0e5392a14d83a5b3d093c750ed5e3903b78f4d9
LypGitHub <657993703@qq.com>
2017-03-22 10:16:54 +0800

    提交

---
+++ scenes/HomeActive.js
@@ -0,0 +1,11 @@
+import React, { Component, View, Text } from 'react-native'
+
+export default class HomeActive extends Component {
+  render () {
+    return (
+      <View>
+        <Text>ss</Text>
+      </View>
+    )
+  }
+}
---
+++ src/routes.js
@@ -0,0 +1,7 @@
+export default {
+  HomeActive: {
+    initialRoute: true,
+    title: '首页',
+    component: require('./scenes/HomeActive').default
+  }
+}
